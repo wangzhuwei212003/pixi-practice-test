b5a18c9fdc5dee86c6986cc854fb5626f79e68e6
zhuwei wang <wangzhuwei212003@163.com>
2018-06-22 12:50:45 +0800

    $ yarn add antd import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import {Menu, Icon, Button, Layout} from 'antd';
 
 import {
   BrowserRouter,
@@ -8,20 +10,88 @@ import {
   Link
 } from 'react-router-dom';
 
+const SubMenu = Menu.SubMenu;
+const {Header, Footer, Sider, Content} = Layout;
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <BrowserRouter>
+          <Layout>
+            <Header>
+              <img src={logo} className="App-logo" alt="logo"/>
+              <h1 className="App-title">Welcome to React</h1>
+              {/*<img src={logo} style={{height: 50}} className="App-logo" alt="logo"/>*/}
+            </Header>
+            <Layout>
+              <Sider>
+                <Menu
+                    defaultSelectedKeys={['1']}
+                    defaultOpenKeys={['sub1']}
+                    mode="inline"
+                    theme="light"
+                    inlineCollapsed={false}
+                >
+                  <Menu.Item key="1">
+                    <Icon type="pie-chart"/>
+                    <span><Link to='/'>Home</Link></span>
+                  </Menu.Item>
+                  <Menu.Item key="2">
+                    <Icon type="desktop"/>
+                    <span><Link to='/MAPF/BasicAStar'>Basic A star</Link></span>
+                  </Menu.Item>
+                  <Menu.Item key="3">
+                    <Icon type="inbox"/>
+                    <span><Link to='/MAPF/Coop4'>Coop 4</Link></span>
+                  </Menu.Item>
+                  <Menu.Item key="4">
+                    <Icon type="inbox"/>
+                    <span><Link to='/MAPF/Coop30'>Coop 30</Link></span>
+                  </Menu.Item>
+                  <Menu.Item key="5">
+                    <Icon type="inbox"/>
+                    <span><Link to='/MAPF/RowByCol'>Rows by columns</Link></span>
+                  </Menu.Item>
+                  <SubMenu key="sub1" title={<span><Icon type="mail"/><span>D3 example</span></span>}>
+                    <Menu.Item key="5"><Link to='/Tutorials/DynamicTable'>DynamicTable</Link></Menu.Item>
+                    <Menu.Item key="6"><Link to='/Tutorials/MakeGrid'>Grid</Link></Menu.Item>
+                    <Menu.Item key="7"><Link to='/Tutorials/mowerDemo'>Mower</Link></Menu.Item>
+                    <Menu.Item key="8"><Link to='/Tutorials/SimpleBarChart'>SimpleBarChart</Link></Menu.Item>
+                    <Menu.Item key="8-1"><Link to='/Tutorials/Tetris'>Tetris</Link></Menu.Item>
+                  </SubMenu>
+                  <SubMenu key="sub2" title={<span><Icon type="appstore"/><span>D3 Util</span></span>}>
+                    <Menu.Item key="9">Option 9</Menu.Item>
+                    <Menu.Item key="10">Option 10</Menu.Item>
+                    <SubMenu key="sub3" title="Mouseover">
+                      <Menu.Item key="11"></Menu.Item>
+                      <Menu.Item key="12">Option 12</Menu.Item>
+                    </SubMenu>
+                  </SubMenu>
+                </Menu>
+              </Sider>
+              <Content>
+                {/*<Route exact path="/" component={Home}/>
+                 <Route path="/example/BarChart" component={BarChart}/>
+                 <Route path="/example/Test" component={Test}/>
+
+                 <Route path="/MAPF/BasicAStar" component={BasicAStar}/>
+                 <Route path="/MAPF/Coop4" component={Coop4}/>
+                 <Route path="/MAPF/Coop30" component={Coop30}/>
+                 <Route path="/MAPF/RowByCol" component={RowByCol}/>
+
+                 <Route path="/Tutorials/DynamicTable" component={DynamicTable}/>
+                 <Route path="/Tutorials/MakeGrid" component={Grid}/>
+                 <Route path="/Tutorials/mowerDemo" component={Mower}/>
+                 <Route path="/Tutorials/SimpleBarChart" component={SimpleBarChart}/>
+                 <Route path="/Tutorials/Tetris" component={Tetris}/>*/}
+
+              </Content>
+            </Layout>
+            <Footer>
+            </Footer>
+          </Layout>
+        </BrowserRouter>
     );
   }
 }
