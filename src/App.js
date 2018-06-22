012842a74c75c0e0443258bbaafa3f803eccad0e
zhuwei wang <wangzhuwei212003@163.com>
2018-06-22 12:40:49 +0800

    yarn  add install the powerful package -- react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
+
 class App extends Component {
   render() {
     return (
