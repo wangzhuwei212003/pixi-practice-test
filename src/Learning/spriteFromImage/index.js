720be2e80e475cf47c553f385856809bfae78707
zhuwei wang <wangzhuwei212003@163.com>
2018-06-22 19:38:50 +0800

    loader image

--- src/Learning/spriteFromImage/index.js
+++ src/Learning/spriteFromImage/index.js
@@ -3,27 +3,27 @@
  */
 import React, {Component} from 'react';
 import * as PIXI from "pixi.js";
+import {Menu, Icon, Button, Layout} from 'antd';
 
-import catImage from '../images/cat.png';
+import catImage from '../images/cat.png'; // 这一步很关键，没有这句话，在下面直接用文件路径是不行的。
+import doorImage from '../images/door.png'; //
 
 export default class spriteFromImage extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
       sprite: null
-    };
-
+    }
   }
 
   componentDidMount() {
-    this.renderer = PIXI.autoDetectRenderer(566, 768);
-
-    this.stage = new PIXI.Container();
-    this.stage.width = 500;
-    this.stage.height = 500;
-    console.log(this.stage);
+    // this.renderer = PIXI.autoDetectRenderer(566, 768);
+    //
+    // this.stage = new PIXI.Container();
+    // this.stage.width = 500;
+    // this.stage.height = 500;
+    // console.log(this.stage);
     //
     // this.isWebGL = this.renderer instanceof PIXI.WebGLRenderer;
     //
@@ -36,49 +36,89 @@ export default class spriteFromImage extends Component {
     //  */
     // this.renderer.view.style['transform'] = 'translatez(0)';
     //
-    this.refs.gameCanvas.appendChild(this.renderer.view);
 
 
+    this.createRenderer();
     this.loadImage();
   }
 
-  loadImage(){
-    PIXI.loader
-        .add(catImage)
-        .load(this.setUp.bind(this));
+  createRenderer() {
+    // let renderer = PIXI.autoDetectRenderer(366, 768);
+    this.renderer = PIXI.autoDetectRenderer(
+        256,                     //Width
+        256,                     //Height
+        {                        //Options
+          antialiasing: false,
+          transparent: false,
+          resolution: 1
+        },
+        false                    //Optionally force canvas rendering
+    );
+
+    //Add the canvas to the HTML document
+    // document.body.appendChild(renderer.view);
+    this.refs.gameCanvas.appendChild(this.renderer.view);
+
+    //Create a container object called the `stage`. The `stage` is the root container for all your objects
+    this.stage = new PIXI.Container();
+
+    //The `renderer.view` is just an ordinary `<canvas>` element.
+    //Here's how you can reference to add an optional dashed
+    //border around the canvas
+    this.renderer.view.style.border = "10px dashed black";
   }
 
-  setUp(){
+  loadImage() {
+    PIXI.loader
+        .add([
+          catImage,
+          doorImage
+        ])
+        .on("progress", this.loadProgressHandler)
+        .load(this.setUp.bind(this));
+  }
+/*.add([
+       {name: 'key4', url: 'http://...', onComplete: function () {} },
+       {url: 'http://...', onComplete: function () {} },
+       'http://...'
+     ]);*/ // 很多用处的 loader
+
+  loadProgressHandler(loader, resource) {
+    //Display the file `url` currently being loaded
+    console.log("loading: " + resource.url);
+
+    //Display the percentage of files currently loaded
+    console.log("progress: " + loader.progress + "%");
+
+    //If you gave your files names as the first argument
+    //of the `add` method, you can access them like this
+    console.log("loading: " + resource.name);
+  }
+  setUp() {
     let cat = new PIXI.Sprite(
         PIXI.loader.resources[catImage].texture
     );
-    console.log(cat);
-
+    // add the cat to the stage
     this.setState({
       sprite: cat
     });
-    console.log(this.state.sprite);
-    // add the cat to the stage
     this.stage.addChild(cat);
-    this.renderer.render(this.stage);
-
+    this.renderer.render(this.stage); // 这个是必须要的，不然显示不出来。
 
     // create a new Sprite from an image path
-    let bunny = PIXI.Sprite.fromImage('../images/cat.png')
+    // let bunny = PIXI.Sprite.fromImage('../images/cat.png') // 这是另一种loadimage的方法。
+  }
 
-    console.log(bunny);
-
-// center the sprite's anchor point
-    bunny.anchor.set(0.5);
-
-// move the sprite to the center of the screen
-    bunny.x = this.renderer.screen.width / 2;
-    bunny.y = this.renderer.screen.height / 2;
-    console.log(bunny.x, bunny.y);
-
-    this.stage.addChild(bunny);
-
+  removeSprite() {
+    console.log('remove sprite');
+    this.stage.removeChild(this.state.sprite);
+    this.stage.removeChildren(); // bug 如果没有这句话，点击两次resume，就remove不掉了。
     this.renderer.render(this.stage);
+  }
+
+  resumeSprite() {
+    console.log('resume sprite');
+    this.setUp();
   }
 
   render() {
@@ -88,9 +128,11 @@ export default class spriteFromImage extends Component {
             1. sayHello occurred 意味着 sayhello 方法生效，<br/>
             2. 一个 black square -》createApplication 方法生效 <br/>
             3. black square变化，-》changeRenderer 生效，<br/>
-            <br/><br/><br/>
-
+            <br/><br/>
           </p>
+          <Button type='dahsed' onClick={this.removeSprite.bind(this)}>remove cat</Button>
+          <Button type='dahsed' onClick={this.resumeSprite.bind(this)}>resume cat</Button>
+          <br/>
         </div>
     );
   }
